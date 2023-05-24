2235badd54c23f8184488c1e4707b39bb70c477d
foxlinkcommunications <134106993+foxlinkcommunications@users.noreply.github.com>
2023-05-24 17:56:03 +0000

    fix hamburger menu to close on clicking the link

--- JS/script.js
+++ JS/script.js
@@ -1,19 +1,29 @@
-const hamburgerMenu = document.querySelector(".hamburger-menu");
-const navLinks = document.querySelector(".nav-links");
-const closeBtn = document.createElement("span");
-closeBtn.innerHTML = "&times;";
-closeBtn.classList.add("close-btn");
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+const navLinks = document.querySelector('.nav-links');
+const navItems = document.querySelectorAll('.nav-links li');
 
-hamburgerMenu.addEventListener("click", () => {
-  navLinks.classList.toggle("nav-active");
-  if (navLinks.classList.contains("nav-active")) {
-    navLinks.appendChild(closeBtn);
-  } else {
-    navLinks.removeChild(closeBtn);
+hamburgerMenu.addEventListener('click', () => {
+  navLinks.classList.toggle('nav-active');
+  navItems.forEach((item, index) => {
+    if (item.style.animation) {
+      item.style.animation = '';
+    } else {
+      item.style.animation = `slideIn 0.5s forwards ${index / 7 + 0.3}s`;
+    }
+  });
+});
+
+// Close the menu and scroll to the section when clicking on a navigation item
+navItems.forEach(item => {
+  item.addEventListener('click', () => {
+    navLinks.classList.remove('nav-active');
+    scrollToSection(item.getAttribute('href'));
+  });
+});
+
+function scrollToSection(target) {
+  const section = document.querySelector(target);
+  if (section) {
+    section.scrollIntoView({ behavior: 'smooth' });
   }
-});
-
-closeBtn.addEventListener("click", () => {
-  navLinks.classList.remove("nav-active");
-  navLinks.removeChild(closeBtn);
-});
+}
