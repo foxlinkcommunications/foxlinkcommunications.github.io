267d9c116c4f6b83ce12ca8a1ca5ccf76d3e2905
foxlinkcommunications <134106993+foxlinkcommunications@users.noreply.github.com>
2023-05-24 16:03:51 +0000

    fix css to add hamburger menu

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,19 @@
 const hamburgerMenu = document.querySelector(".hamburger-menu");
 const navLinks = document.querySelector(".nav-links");
+const closeBtn = document.createElement("span");
+closeBtn.innerHTML = "&times;";
+closeBtn.classList.add("close-btn");
 
 hamburgerMenu.addEventListener("click", () => {
   navLinks.classList.toggle("nav-active");
+  if (navLinks.classList.contains("nav-active")) {
+    navLinks.appendChild(closeBtn);
+  } else {
+    navLinks.removeChild(closeBtn);
+  }
+});
+
+closeBtn.addEventListener("click", () => {
+  navLinks.classList.remove("nav-active");
+  navLinks.removeChild(closeBtn);
 });
